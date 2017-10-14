8f8a33c2077f5fd1abd927270a39660970c1987d
Emily Mattison <egmatti@gmail.com>
2017-10-14 17:58:57 -0400

    Added content and styling to Home page

---
+++ js/home.js
@@ -0,0 +1,7 @@
+// ==========================================
+// HOME PAGE
+// ==========================================
+
+$("#home-page .round-button").click(function() {
+   $("#home-page .round-button").css({"background-color": "#fff", "color": "#e9a92f"});
+});
