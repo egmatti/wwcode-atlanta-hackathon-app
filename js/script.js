f3f721aa3876717639a2a8dbe87536bdba8a7f89
Emily Mattison <egmatti@gmail.com>
2017-10-14 21:18:55 -0400

    Added and styled menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// MENU
+
+$(".arrow-white").click(function() {
+  $(".arrow-white").css({"display": "none"});
+  $(".arrow-black").css({"display": "inline-block"});
+});
+
+$(".hamburger-menu-container").click(function() {
+   $(".menu").slideToggle();
+   $(".hamburger-menu-container").css({"display": "none"});
+   $(".x").css({"display": "inline-block"});
+   $("body").css({"overflow": "hidden"});
+});
+
+$(".x").click(function() {
+   $(".menu").slideToggle();
+   $(".hamburger-menu-container").css({"display": "flex"});
+   $(".x").css({"display": "none"});
+   $("body").css({"overflow": "visible"});
+});
+
+
+// BUTTON COLOR CHANGE
+
 $(".round-button").click(function() {
    $(".round-button").css({"background-color": "#e9a92f", "color": "#fff"});
+});
+
+$(".menu .round-button").click(function() {
+   $(".menu .round-button").css({"background-color": "#fff", "color": "#e9a92f"});
 });
